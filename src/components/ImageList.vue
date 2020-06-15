f193ef3b1457b694d59965f5e985a835a3f802d3
GNeto <glaudistonson@gmail.com>
2020-06-15 20:39:59 -0300

    finalized with error

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <img v-for="image in allImages" :src="image.link" :key="image.id" />
+    <div v-if="isLoggedIn" class="image-container"> <!-- not to use isLoggedIn on begrato -->
+      <img v-for="image in allImages" :src="image.link" :key="image.id" />
+    </div>
+    <h2 v-else>Log in to get started</h2>
   </div>
 </template>
 
@@ -9,14 +12,21 @@ import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'ImageList',  
-  methods: mapActions(['fetchImages']),
+  methods: mapActions(['fetchImages']), // not to use isLoggedIn on begrato
   created() {
     this.fetchImages();
   },
-  computed: mapGetters(['allImages'])
+  computed: mapGetters(['allImages', 'isLoggedIn'])
 }
 </script>
 
-<style>
-
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+img {
+  max-width: 100%;
+  padding: 5px;
+}
 </style>
